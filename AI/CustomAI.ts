49898baead3d852919462615768d1a7c284283dd
fountainstudios <51059688+fountainstudios@users.noreply.github.com>
2020-07-08 20:50:38 +0200

    Update AI/CustomAI.ts

--- AI/CustomAI.ts
+++ AI/CustomAI.ts
@@ -1,12 +1,16 @@
+enum PatternType {
+    //% block="Spotted"
+    Spotted,
+    //% block="Striped"
+    Striped
+    }
+
 //% color=#00bbff
 namespace AIForEarth{
 
-    /**
-     * Test
-     * @param value, the number of sequence loops
-     */
-    //% block
-    export function testSequence(value: number){
+
+    //% block="sequence %value|"
+    export function test1(value: number){
         let i
         for (i = 0; i < value; i++) {
             agent.move(FORWARD, 3)
@@ -19,40 +23,45 @@ namespace AIForEarth{
             agent.turn(TurnDirection.Right)
         }
     }
-    /**
-     * Test2
-     * @param value the number of sequence loops
-     */
-    //% block
-    export function sequenceTest(value: number){
-        let i
-        for (i = 0; i < value; i++) {
-            agent.move(FORWARD, 3)
-            agent.turn(TurnDirection.Right)
-            agent.move(FORWARD, 3)
-            agent.turn(TurnDirection.Right)
-            agent.move(FORWARD, 3)
-            agent.turn(TurnDirection.Right)
-            agent.move(FORWARD, 3)
-            agent.turn(TurnDirection.Right)
-        }
+        
+    //% block="run pattern || %patternType|at %speed|for %duration ms"
+    //% duration.shadow=timePicker
+    //% speed.min=0 speed.max=60
+    //% expandableArgumentMode="enabled"
+    export function test2(direction: PatternType,
+        speed: number,
+        duration: number){
+            /*Code Random*/
     }
-    /**
-     * Test3
-     * @param value, the number of sequence loop
-     */
-    //% block
-    export function sequence(value: number){
-        let i
-        for (i = 0; i < value; i++) {
-            agent.move(FORWARD, 3)
-            agent.turn(TurnDirection.Right)
-            agent.move(FORWARD, 3)
-            agent.turn(TurnDirection.Right)
-            agent.move(FORWARD, 3)
-            agent.turn(TurnDirection.Right)
-            agent.move(FORWARD, 3)
-            agent.turn(TurnDirection.Right)
-        }
+
+    //% block="run pattern variation || %patternType|at %speed|for %duration ms"
+    //% duration.shadow=timePicker
+    //% speed.min=0 speed.max=100
+    //% expandableArgumentMode="toggle"
+    export function test3(direction: PatternType,
+        speed: number,
+        duration: number){
+            /*Code Random*/
+    }
+
+    //% block="tpTypeBlockExample"
+    export function test4(){
+            /*Code Random*/
+    }
+    
+    //% block="return number"
+    export function test5(): number {
+        return 0;
+    }
+
+    //% block="return boolean"
+    export function test6(): boolean {
+        return false;
+    }
+
+
+    //% block="on event"
+    export function test7(handler: () => void) {
+
     }
 }
